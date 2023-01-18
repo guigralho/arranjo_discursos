e6f979bd7f6a3244a787a37c3c983fb5da201a75
Guilherme Gralho <guilherme.gralho@interaktiv.com.br>
2023-01-18 19:04:49 -0300

    Ajusta layout

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -80,11 +80,11 @@ export default {
             </label>
         </div>
 
-        <div class="mt-4 flex items-center justify-end">
+        <div class="mt-4 flex flex-wrap items-center justify-end gap-3">
             <Link
                 v-if="canResetPassword"
                 :href="route('password.request')"
-                class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800"
+                class="flex-auto rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800"
             >
                 Esqueceu sua senha?
             </Link>
@@ -92,7 +92,7 @@ export default {
             <PrimaryButton
                 :class="{ 'opacity-25': form.processing }"
                 :href="route('register')"
-                class="ml-4"
+                class="flex-auto"
             >
                 Cadastrar-se
             </PrimaryButton>
@@ -100,7 +100,7 @@ export default {
             <PrimaryButton
                 :class="{ 'opacity-25': form.processing }"
                 :disabled="form.processing"
-                class="ml-4"
+                class="flex-auto"
             >
                 Entrar
             </PrimaryButton>
