44b27e04bb20fc6b66367541e76cd05aae3fd5f9
Guilherme Gralho <guilherme.gralho@connectcom.com.br>
2025-09-26 18:04:22 -0300

    Adiciona meta tags para cores e status bar em iOS/Safari

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Safari/iOS Status Bar and Navigation Colors -->
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="theme-color" content="#1f2937">
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
